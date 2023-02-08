fd9aaa678e4bb25cd3d50b3952a006ff240b4c9d
Ewoud Surmont <surmontewoud@gmail.com>
2023-02-08 20:55:45 +0000

    Setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+				'emoji': ["Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+			},
+			colors: {
+				'brand-green': '#005D5F',
+				'brand-green-dark': '#004E50',
+			},
+		},
 	},
 	plugins: [],
 }
