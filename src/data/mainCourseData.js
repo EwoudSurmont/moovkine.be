94c219f9fcd66775c7c91a07193e2a2011505144
Ewoud Surmont <surmontewoud@gmail.com>
2023-02-24 12:45:15 +0000

    Cleanup

---
+++ src/data/mainCourseData.js
@@ -0,0 +1,69 @@
+const mainCourseData = [
+	{
+		title: "Master of science in de Revalidatiewetenschappen en kinesitherapie, afstudeerrichting sportrevalidatie", 
+		year: "2013",
+		school: "KU Leuven",
+	},
+	{
+		title: "Bijzondere beroepsbekwaamheid Manuele Therapie, postgraduaat Manuele therapie", 
+		year: "2014",
+		school:"UGent",
+	},
+	{
+		title: "Sportspecifiek letseldetectie- en preventieprogramma", 
+		year: "2011",
+		school:"Bloso",
+	},
+	{
+		title: "Sporttaping", 
+		year: "2012",
+		school:"HUB",
+	},
+	{
+		title: "De schouder bij de sporter – nieuwe inzichten", 
+		year: "2015",
+		school:"Brucosport",
+	},
+	{
+		title: "Myofasciale therapie (dry needling)", 
+		year: "2016",
+		school:"VZW Trigger/BMST",
+	},
+	{
+		title: "Spinale segment sensitisatie en technieken van desensitisatie", 
+		year: "2017",
+		school:"VZW Trigger/BMST",
+	},
+	{
+		title: "Kinecoach® obesitas bij volwassenen, adolescenten en kinderen", 
+		year: "2018",
+		school:"n.v.t.",
+	},
+	{
+		title: "Overzicht van respiratoire kinesitherapeutische technieken bij baby’s", 
+		year: "2018",
+		school:"IPG/CF-Belgium",
+	},
+	{
+		title: "Masterclass Dry Needling Jan Dommerholt", 
+		year: "2018",
+		school:"VZW Trigger/BMST",
+	},
+	{
+		title: "Kinecoach® diabetes type 2", 
+		year: "2019",
+		school:"n.v.t.",
+	},
+	{
+		title: "Patellofemoral pain: Hoffa’s fat pad", 
+		year: "2020",
+		school:"SmartEducation",
+	},
+	{
+		title: "Masterclass Subacromiale schouderpijn", 
+		year: "2021",
+		school:"SmartEducation",
+	},
+]
+
+export default mainCourseData;
