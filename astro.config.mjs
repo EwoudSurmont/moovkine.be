f1dbc5c9bb363df4d310d926be5cde58b5ffd292
Ewoud Surmont <surmontewoud@gmail.com>
2023-02-24 14:02:58 +0000

    Image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,12 @@ import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()],
-  site: 'https://www.moovkine.be'
+  integrations: [tailwind(), prefetch(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  site: 'https://www.moovkine.be',
 });
