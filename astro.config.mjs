19e336578253f5a48e8193aaec4434e4148febc0
Ewoud Surmont <surmontewoud@gmail.com>
2024-10-29 19:26:14 +0100

    Database setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), prefetch()],
   site: 'https://www.moovkine.be',
+  output: "server",
+  adapter: netlify(),
 });
