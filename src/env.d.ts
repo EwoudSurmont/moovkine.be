63d63ca539047b1b4498eb8b9044964d9511e895
Ewoud Surmont <surmontewoud@gmail.com>
2025-10-22 16:29:25 +0200

    feat: Replace LinkButton with AppointmentDialog component in index page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,24 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Astro component module declarations
+declare module '*.astro' {
+  const Component: any;
+  export default Component;
+}
+
+// TypeScript module declarations for data files
+declare module '../data/navData' {
+  const navData: any;
+  export default navData;
+}
+
+declare module '../data/teamData' {
+  const teamData: any;
+  export default teamData;
+}
+
+declare module '../data/courseData' {
+  const courseData: any;
+  export default courseData;
+}
